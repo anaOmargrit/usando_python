bbca98705328230fe299bb655ea4d927db2d23c9
Mariana Blanco <69396280+anaOmargrit@users.noreply.github.com>
2021-09-22 15:50:59 -0300

    Create traducir_a_geringoso.py

---
+++ traducir_a_geringoso.py
@@ -0,0 +1,23 @@
+'''
+Recibe una lista de palabras y genera un diccionario de las mismas palabras traducidas al geringoso.
+'''
+lista = ['banana', 'manzana', 'mandarina']
+
+def diccionario_geringoso(lista):
+    nueva_lista = []    
+    
+    for palabra in lista:
+        npalabra = ''        
+        for c in palabra:
+            npalabra += c
+            
+            if c in 'aeiou':
+                npalabra += 'p' + c
+            
+            elif c in 'AEIOU':
+                npalabra += 'P' + c
+        
+        nueva_lista.append(npalabra)
+    d = dict(zip(lista, nueva_lista))
+    
+    return d
